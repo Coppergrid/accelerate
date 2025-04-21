7509ca928f96acc5382f7d38fdf00cded20b6c7f
OneLinkPog <134339728+OneLinkPog@users.noreply.github.com>
2025-04-21 17:07:56 -0400

    Reset gradle to fix mod launching issues

--- common/src/main/java/net/createmaglev/MaglevBlocks.java
+++ common/src/main/java/net/createmaglev/MaglevBlocks.java
@@ -1,19 +1,20 @@
 package net.createmaglev;
 
+import com.simibubi.create.AllBlocks;
+import com.simibubi.create.content.trains.track.TrackMaterial;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.material.*;
+import net.minecraft.world.level.material.MapColor;
 import com.simibubi.create.content.trains.track.TrackBlock;
 
 public class MaglevBlocks {
 	public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(CreateMaglev.MOD_ID);
 
-	public static final BlockEntry<TrackBlock> MAGLEV_RAIL_BLOCK = REGISTRATE.block("maglev_rail", properties -> new TrackBlock(Block.Properties.of(Material.METAL).strength(2.0f))).register();
+	public static final BlockEntry<TrackBlock> MAGLEV_RAIL_BLOCK = REGISTRATE.block("maglev_rail", properties -> new TrackBlock(Block.Properties.of().strength(2.0f), TrackMaterial.ANDESITE)).register();
 
 	public static void init() {
 		// load the class and register everything
 		CreateMaglev.LOGGER.info("Registering blocks for " + CreateMaglev.NAME);
-		MAGLEV_RAIL_BLOCK.get();
 	}
 }
---
+++ common/src/main/java/net/createmaglev/MaglevTrackBlock.java
@@ -0,0 +1,10 @@
+package net.createmaglev;
+
+import com.simibubi.create.content.trains.track.TrackBlock;
+import com.simibubi.create.content.trains.track.TrackMaterial;
+
+public class MaglevTrackBlock extends TrackBlock {
+    public MaglevTrackBlock(Properties properties) {
+        super(properties, TrackMaterial.ANDESITE);
+    }
+}
