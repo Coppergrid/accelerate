e746f889266f03920d35c93ec5d9e2f1220f6156
OneLinkPog <134339728+OneLinkPog@users.noreply.github.com>
2025-05-18 10:40:47 -0400

    Reformatted rail tags to fit with Create Mod's format

--- common/src/main/java/net/createmaglev/MaglevBlocks.java
+++ common/src/main/java/net/createmaglev/MaglevBlocks.java
@@ -1,6 +1,10 @@
 package net.createmaglev;
 
 import com.simibubi.create.AllBlocks;
+import com.simibubi.create.content.trains.track.TrackBlock;
+import com.simibubi.create.content.trains.track.TrackBlockItem;
+import com.simibubi.create.AllTags.AllBlockTags;
+import com.simibubi.create.AllTags.AllItemTags;
 import com.simibubi.create.content.trains.track.TrackMaterial;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.tterrag.registrate.util.entry.BlockEntry;
@@ -11,7 +15,14 @@ import com.simibubi.create.content.trains.track.TrackBlock;
 public class MaglevBlocks {
 	public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(CreateMaglev.MOD_ID);
 
-	public static final BlockEntry<TrackBlock> MAGLEV_RAIL_BLOCK = REGISTRATE.block("maglev_rail", properties -> new TrackBlock(Block.Properties.of().strength(2.0f), TrackMaterial.ANDESITE)).register();
+	public static final BlockEntry<TrackBlock> MAGLEV_RAIL_BLOCK = REGISTRATE.block("maglev_rail", properties -> new TrackBlock(Block.Properties.of().strength(2.0f), TrackMaterial.ANDESITE))
+			.tag(AllBlockTags.RELOCATION_NOT_SUPPORTED.tag)
+			.tag(AllBlockTags.TRACKS.tag)
+			.tag(AllBlockTags.GIRDABLE_TRACKS.tag)
+			.lang("Maglev Track")
+			.item(TrackBlockItem::new)
+			.build()
+			.register();
 
 	public static void init() {
 		// load the class and register everything
