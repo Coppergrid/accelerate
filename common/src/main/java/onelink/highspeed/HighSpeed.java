76b952404bd6df6e4fb1831cfa810f8559935afc
OneLinkPog <134339728+OneLinkPog@users.noreply.github.com>
2025-08-10 22:46:39 -0400

    v6 stuff

--- common/src/main/java/onelink/highspeed/HighSpeed.java
+++ common/src/main/java/onelink/highspeed/HighSpeed.java
@@ -9,10 +9,11 @@ public class HighSpeed {
     public static final String MOD_ID = "highspeed";
     public static final String NAME = "Create: Highspeed";
     public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
+    public static final String VERSION = "0.0.1 ALPHA";
 
 
     public static void init() {
-        LOGGER.info("{} initializing! Create version: {} on platform: {}", NAME, Create.VERSION, HSExpectPlatform.platformName());
+        LOGGER.info("{} initializing! Create version: {} on platform: {}", NAME, HighSpeed.VERSION, HSExpectPlatform.platformName());
         HSBlocks.init(); // hold registrate in a separate class to avoid loading early on forge
         HSItems.init(); // hold registrate in a separate class to avoid loading early on forge
     }
