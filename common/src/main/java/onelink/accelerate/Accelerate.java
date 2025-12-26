59325a39aed8ad9526ffed07027cd6dc21a657d4
OneLinkPog <134339728+OneLinkPog@users.noreply.github.com>
2025-12-26 11:42:06 +0000

    Modify initialization message

--- common/src/main/java/onelink/accelerate/Accelerate.java
+++ common/src/main/java/onelink/accelerate/Accelerate.java
@@ -1,7 +1,6 @@
 package onelink.accelerate;
 
 import com.simibubi.create.foundation.data.CreateRegistrate;
-import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +13,7 @@ public class Accelerate {
 
 
     public static void init() {
-        LOGGER.info("{} initializing! Create version: {} on platform: {}", NAME, Accelerate.VERSION, AExpectPlatform.platformName());
+        LOGGER.info("{} initializing! Version {} on {} loader.", NAME, Accelerate.VERSION, AExpectPlatform.platformName());
         ABlocks.init(); // hold registrate in a separate class to avoid loading early on forge
         AItems.init(); // hold registrate in a separate class to avoid loading early on forge
     }
