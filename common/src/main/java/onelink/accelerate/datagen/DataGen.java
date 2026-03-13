7d876030a826a433e2c5394d5320b8b6309c1bc7
OneLinkPog <134339728+OneLinkPog@users.noreply.github.com>
2026-03-13 17:07:39 -0400

    Remove unused imports

--- common/src/main/java/onelink/accelerate/datagen/DataGen.java
+++ common/src/main/java/onelink/accelerate/datagen/DataGen.java
@@ -1,8 +1,5 @@
 package onelink.accelerate.datagen;
 
-import com.tterrag.registrate.providers.ProviderType;
-import onelink.accelerate.Accelerate;
-
 
 public class DataGen {
     public static void register() {
