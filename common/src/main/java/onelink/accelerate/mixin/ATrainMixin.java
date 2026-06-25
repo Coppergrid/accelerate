5041bf13312f2090c6e7c62747d7be4220cb5a95
OneLinkPog <134339728+OneLinkPog@users.noreply.github.com>
2026-06-24 22:34:04 -0400

    Fix compilation issue

--- common/src/main/java/onelink/accelerate/mixin/ATrainMixin.java
+++ common/src/main/java/onelink/accelerate/mixin/ATrainMixin.java
@@ -6,6 +6,7 @@ import onelink.accelerate.config.AConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(value = Train.class)
 public abstract class ATrainMixin {
@@ -13,7 +14,11 @@ public abstract class ATrainMixin {
     @Shadow
     public int fuelTicks;
 
+    @Unique
+    boolean create_accelerate$onHighSpeedTrack = true;
 
+    @Unique
+    boolean create_accelerate$onMaglevTrack = false;
     /**
      * @author OneLink
      * @reason Allow custom tracks to return higher speeds
@@ -25,11 +30,11 @@ public abstract class ATrainMixin {
                 ? AllConfigs.server().trains.poweredTrainAcceleration.getF()
                 : AllConfigs.server().trains.trainAcceleration.getF());
 
-        if (this.onHighSpeedTrack()) { // Add methods to integrate properly
+        if (create_accelerate$onHighSpeedTrack) { // Add methods to integrate properly
             return (float) ((baseAccel * AConfig.COMMON.HighSpeedTrackMultiplier.get()) / 400);
         }
 
-        else if (this.onMaglevTrack()) { // See above
+        else if (create_accelerate$onMaglevTrack) { // See above
             return (float) ((baseAccel * AConfig.COMMON.MaglevTrackMultiplier.get()) / 400);
         }
 
