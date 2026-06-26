dbe61a88f9f0bce625562416f0d664f38bf1bc60
OneLinkPog <134339728+OneLinkPog@users.noreply.github.com>
2026-06-25 20:55:02 -0400

    Remove config maximums

--- common/src/main/java/onelink/accelerate/config/AConfig.java
+++ common/src/main/java/onelink/accelerate/config/AConfig.java
@@ -14,15 +14,15 @@ public class AConfig {
             builder.push("Train Stats");
             builder.push("Acceleration");
             HighSpeedAccelMultiplier = builder.comment("Acceleration Multiplier for High Speed Tracks and Trains")
-                    .defineInRange("highSpeedAccelMultiplier", 1.5, 0.1, 10);
+                    .defineInRange("highSpeedAccelMultiplier", 1.5, 0.1, Double.MAX_VALUE);
             MaglevAccelMultiplier = builder.comment("Acceleration Multiplier for Maglev Tracks and Trains")
-                    .defineInRange("maglevAccelMultiplier", 2.5, 0.1, 10);
+                    .defineInRange("maglevAccelMultiplier", 2.5, 0.1, Double.MAX_VALUE);
             builder.pop();
             builder.push("Speed");
             HighSpeedSpeedMultiplier = builder.comment("Speed Multiplier for High Speed Tracks and Trains")
-                    .defineInRange("highSpeedSpeedMultiplier", 2, 0.1, 10);
+                    .defineInRange("highSpeedSpeedMultiplier", 2, 0.1, Double.MAX_VALUE);
             MaglevSpeedMultiplier = builder.comment("Speed Multiplier for Maglev Tracks and Trains")
-                    .defineInRange("maglevSpeedMultiplier", 3, 0.1, 10);
+                    .defineInRange("maglevSpeedMultiplier", 3, 0.1, Double.MAX_VALUE);
             builder.pop();
             builder.pop();
         }
