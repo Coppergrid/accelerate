31f94d831a87088b95315b5e2eda09b443e17567
OneLinkPog <134339728+OneLinkPog@users.noreply.github.com>
2026-07-13 17:54:44 -0400

    Add asResource function

--- common/src/main/java/onelink/accelerate/Accelerate.java
+++ common/src/main/java/onelink/accelerate/Accelerate.java
@@ -1,10 +1,13 @@
 package onelink.accelerate;
 
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import net.minecraft.resources.ResourceLocation;
 import onelink.accelerate.registry.ABlocks;
 import onelink.accelerate.registry.AItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
 
 
 public class Accelerate {
@@ -14,6 +17,9 @@ public class Accelerate {
     public static final String VERSION = "0.0.1 ALPHA";
     public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(Accelerate.MOD_ID).defaultCreativeTab(ACreativeTabs.getBaseTab());
 
+    public static ResourceLocation asResource(@Nonnull String path) {
+        return new ResourceLocation(MOD_ID, path);
+    }
 
     public static void init() {
         LOGGER.info("{} initializing! Version {} on {} loader.", NAME, Accelerate.VERSION, AExpectPlatform.platformName());
