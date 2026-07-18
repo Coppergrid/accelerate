8e24e11d719faadc89fddb3ab25484319ee97974
CircuitScoutDev <134339728+CircuitScoutDev@users.noreply.github.com>
2026-07-18 17:25:51 -0400

    Add fromBogey method

--- common/src/main/java/coppergrid/accelerate/infrastructure/train/TrainType.java
+++ common/src/main/java/coppergrid/accelerate/infrastructure/train/TrainType.java
@@ -1,9 +1,22 @@
 package coppergrid.accelerate.infrastructure.train;
 
+import com.simibubi.create.content.trains.bogey.AbstractBogeyBlock;
+import coppergrid.accelerate.content.bogey.HighSpeedBogeyBlock;
+
 public enum TrainType {
     NORMAL,
     HIGH_SPEED,
-    MAGLEV
+    MAGLEV;
+
+    public static TrainType fromBogey(AbstractBogeyBlock<?> bogey) {
+        if (bogey instanceof HighSpeedBogeyBlock)
+            return HIGH_SPEED;
+
+//        if (bogey instanceof MaglevBogeyBlock)
+//            return MAGLEV;
+
+        return NORMAL;
+    }
 }
 
 
