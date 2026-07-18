392f9ad065ef64c67eb2eb465b008cc7443aabd8
CircuitScoutDev <134339728+CircuitScoutDev@users.noreply.github.com>
2026-07-18 19:16:06 -0400

    Add TrainType helper functions

--- common/src/main/java/coppergrid/accelerate/infrastructure/train/TrainType.java
+++ common/src/main/java/coppergrid/accelerate/infrastructure/train/TrainType.java
@@ -1,6 +1,7 @@
 package coppergrid.accelerate.infrastructure.train;
 
 import com.simibubi.create.content.trains.bogey.AbstractBogeyBlock;
+import coppergrid.accelerate.config.AConfig;
 import coppergrid.accelerate.content.bogey.HighSpeedBogeyBlock;
 
 public enum TrainType {
@@ -16,6 +17,23 @@ public enum TrainType {
 //            return MAGLEV;
 
         return NORMAL;
+    }
+
+    public double accelerationMultiplier() {
+        return switch (this) {
+            case HIGH_SPEED -> AConfig.SERVER.HighSpeedAccelMultiplier.get();
+            case MAGLEV -> AConfig.SERVER.MaglevAccelMultiplier.get();
+            case null, default -> 1.0;
+        };
+    }
+
+    public double speedMultiplier() {
+        return switch (this) {
+            case NORMAL -> 1.0;
+            case HIGH_SPEED -> AConfig.SERVER.HighSpeedSpeedMultiplier.get();
+            case MAGLEV -> AConfig.SERVER.MaglevSpeedMultiplier.get();
+
+        };
     }
 }
 
