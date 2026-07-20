29d52ed5e80570e9b1c6eb96761e528297b74a08
CircuitScoutDev <134339728+CircuitScoutDev@users.noreply.github.com>
2026-07-20 18:16:30 -0400

    Remove unused imports

--- common/src/main/java/coppergrid/accelerate/mixin/TrainStatsMixin.java
+++ common/src/main/java/coppergrid/accelerate/mixin/TrainStatsMixin.java
@@ -2,12 +2,10 @@ package coppergrid.accelerate.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.simibubi.create.content.trains.entity.Train;
-import coppergrid.accelerate.config.AConfig;
 import coppergrid.accelerate.infrastructure.track.TrackHelper;
 import coppergrid.accelerate.infrastructure.train.TrainType;
 import coppergrid.accelerate.infrastructure.train.TrainTypeStorage;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = Train.class, remap = false)
