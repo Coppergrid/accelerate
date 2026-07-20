4808334267c385ec610118870c878091c7ea26de
CircuitScoutDev <134339728+CircuitScoutDev@users.noreply.github.com>
2026-07-20 17:49:25 -0400

    Add speed multiplier integrated method

--- common/src/main/java/coppergrid/accelerate/infrastructure/track/TrackHelper.java
+++ common/src/main/java/coppergrid/accelerate/infrastructure/track/TrackHelper.java
@@ -2,19 +2,38 @@ package coppergrid.accelerate.infrastructure.track;
 
 import com.simibubi.create.content.trains.entity.Train;
 import com.simibubi.create.content.trains.track.TrackMaterial;
+import coppergrid.accelerate.config.AConfig;
+import coppergrid.accelerate.registry.ATrackMaterials;
+import org.apache.commons.lang3.mutable.MutableDouble;
 
 import java.util.concurrent.atomic.AtomicReference;
 
 public class TrackHelper {
-    public static TrackMaterial getCurrentTrackMaterial(Train train) {
-        AtomicReference<TrackMaterial> material = new AtomicReference<>();
+
+    public static double getSpeedMultiplier(TrackMaterial trackMaterial) {
+        if (trackMaterial == ATrackMaterials.HIGH_SPEED)
+            return AConfig.SERVER.HighSpeedSpeedMultiplier.get();
+
+        if (trackMaterial == ATrackMaterials.MAGLEV)
+            return AConfig.SERVER.MaglevSpeedMultiplier.get();
+
+        return 1.0;
+    }
+
+    public static double getCurrentTrackSpeedMultiplier(Train train) {
+        MutableDouble multiplier = new MutableDouble(Double.MAX_VALUE);
 
         train.forEachTravellingPoint(tp -> {
 
-            material.set(tp.edge.getTrackMaterial());
+            if (tp.edge == null)
+                return;
+
+            double current = getSpeedMultiplier(tp.edge.getTrackMaterial());
+
+            multiplier.setValue(Math.min(multiplier.getValue(), current));
 
         });
 
-        return material.get();
+        return multiplier.getValue() == Double.MAX_VALUE ? 1.0 : multiplier.getValue();
     }
 }
