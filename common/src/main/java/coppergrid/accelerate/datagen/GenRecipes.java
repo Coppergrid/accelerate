cf16122e419068f486c1208e7a1f853e580feac3
CircuitScoutDev <134339728+CircuitScoutDev@users.noreply.github.com>
2026-07-22 08:36:49 -0400

    Datagen Automated Driver and ADU

--- common/src/main/java/coppergrid/accelerate/datagen/GenRecipes.java
+++ common/src/main/java/coppergrid/accelerate/datagen/GenRecipes.java
@@ -1,9 +1,11 @@
 package coppergrid.accelerate.datagen;
 
+import com.simibubi.create.AllBlocks;
 import com.simibubi.create.AllItems;
 import com.tterrag.registrate.providers.ProviderType;
 import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import coppergrid.accelerate.Accelerate;
+import coppergrid.accelerate.registry.ABlocks;
 import coppergrid.accelerate.registry.AItems;
 import net.minecraft.advancements.critereon.InventoryChangeTrigger;
 import net.minecraft.advancements.critereon.ItemPredicate;
@@ -25,7 +27,29 @@ public class GenRecipes {
     }
 
     public static void generateRecipes(RegistrateRecipeProvider recipeProvider) {
-        
+        ShapedRecipeBuilder
+                .shaped(RecipeCategory.TRANSPORTATION, AItems.AUTOMATED_DRIVER)
+                .unlockedBy("has_item", unlockedByItem(AllItems.PRECISION_MECHANISM))
+                .define('A', AllItems.ANDESITE_ALLOY)
+                .define('G', AllItems.GOLDEN_SHEET)
+                .define('P', AllItems.PRECISION_MECHANISM)
+                .define('Q', AllItems.POLISHED_ROSE_QUARTZ)
+                .pattern("AGA")
+                .pattern("QPQ")
+                .pattern("AGA")
+                .save(recipeProvider);
+
+        ShapedRecipeBuilder
+                .shaped(RecipeCategory.TRANSPORTATION, ABlocks.AUTOMATED_DRIVING_MODULE.asItem())
+                .unlockedBy("has_item", unlockedByItem(AItems.AUTOMATED_DRIVER))
+                .define('C', AItems.CHARGED_COBALT_INGOT)
+                .define('D', AItems.AUTOMATED_DRIVER)
+                .define('S', AllItems.STURDY_SHEET)
+                .define('T', AllBlocks.RAILWAY_CASING.asItem())
+                .pattern("SCS")
+                .pattern("TDT")
+                .pattern("SCS")
+                .save(recipeProvider);
     }
 
     public static void register() {
