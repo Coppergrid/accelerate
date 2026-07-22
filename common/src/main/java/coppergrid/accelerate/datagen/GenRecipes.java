496c357e2129cfda97d707e629d2b8282b7590df
CircuitScoutDev <134339728+CircuitScoutDev@users.noreply.github.com>
2026-07-22 09:03:05 -0400

    Move crafting recipes to separate file

--- common/src/main/java/coppergrid/accelerate/datagen/GenRecipes.java
+++ common/src/main/java/coppergrid/accelerate/datagen/GenRecipes.java
@@ -1,58 +1,12 @@
 package coppergrid.accelerate.datagen;
 
-import com.simibubi.create.AllBlocks;
-import com.simibubi.create.AllItems;
 import com.tterrag.registrate.providers.ProviderType;
-import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import coppergrid.accelerate.Accelerate;
-import coppergrid.accelerate.registry.ABlocks;
-import coppergrid.accelerate.registry.AItems;
-import net.minecraft.advancements.critereon.InventoryChangeTrigger;
-import net.minecraft.advancements.critereon.ItemPredicate;
-import net.minecraft.data.recipes.RecipeCategory;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.ItemLike;
-
-import static com.tterrag.registrate.providers.RegistrateRecipeProvider.inventoryTrigger;
+import coppergrid.accelerate.datagen.recipe.GenCraftingRecipes;
 
 public class GenRecipes {
-    private static InventoryChangeTrigger.TriggerInstance unlockedByItem(ItemLike item) {
-        return inventoryTrigger(ItemPredicate.Builder.item().of(item).build());
-    }
-
-    private static InventoryChangeTrigger.TriggerInstance unlockedByItemTag(TagKey<Item> tag) {
-        return inventoryTrigger(ItemPredicate.Builder.item().of(tag).build());
-    }
-
-    public static void generateRecipes(RegistrateRecipeProvider recipeProvider) {
-        ShapedRecipeBuilder
-                .shaped(RecipeCategory.TRANSPORTATION, AItems.AUTOMATED_DRIVER)
-                .unlockedBy("has_item", unlockedByItem(AllItems.PRECISION_MECHANISM))
-                .define('A', AllItems.ANDESITE_ALLOY)
-                .define('G', AllItems.GOLDEN_SHEET)
-                .define('P', AllItems.PRECISION_MECHANISM)
-                .define('Q', AllItems.POLISHED_ROSE_QUARTZ)
-                .pattern("AGA")
-                .pattern("QPQ")
-                .pattern("AGA")
-                .save(recipeProvider);
-
-        ShapedRecipeBuilder
-                .shaped(RecipeCategory.TRANSPORTATION, ABlocks.AUTOMATED_DRIVING_MODULE.asItem())
-                .unlockedBy("has_item", unlockedByItem(AItems.AUTOMATED_DRIVER))
-                .define('C', AItems.CHARGED_COBALT_INGOT)
-                .define('D', AItems.AUTOMATED_DRIVER)
-                .define('S', AllItems.STURDY_SHEET)
-                .define('T', AllBlocks.RAILWAY_CASING.asItem())
-                .pattern("SCS")
-                .pattern("TDT")
-                .pattern("SCS")
-                .save(recipeProvider);
-    }
 
     public static void register() {
-        Accelerate.REGISTRATE.addDataGenerator(ProviderType.RECIPE, GenRecipes::generateRecipes);
+        Accelerate.REGISTRATE.addDataGenerator(ProviderType.RECIPE, GenCraftingRecipes::generateRecipes);
     }
 }
