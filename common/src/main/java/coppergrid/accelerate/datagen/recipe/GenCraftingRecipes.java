38744ae2b8d6c39fdab916cd43237ed769b081f6
CircuitScoutDev <134339728+CircuitScoutDev@users.noreply.github.com>
2026-07-24 11:28:52 -0400

    Remove helper functions

--- common/src/main/java/coppergrid/accelerate/datagen/recipe/GenCraftingRecipes.java
+++ common/src/main/java/coppergrid/accelerate/datagen/recipe/GenCraftingRecipes.java
@@ -6,31 +6,17 @@ import com.tterrag.registrate.providers.RegistrateRecipeProvider;
 import coppergrid.accelerate.Accelerate;
 import coppergrid.accelerate.registry.ABlocks;
 import coppergrid.accelerate.registry.AItems;
-import net.minecraft.advancements.critereon.InventoryChangeTrigger;
-import net.minecraft.advancements.critereon.ItemPredicate;
 import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.data.recipes.ShapelessRecipeBuilder;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.ItemLike;
-
-import static com.tterrag.registrate.providers.RegistrateRecipeProvider.inventoryTrigger;
 
 public class GenCraftingRecipes {
-    private static InventoryChangeTrigger.TriggerInstance unlockedByItem(ItemLike item) {
-        return inventoryTrigger(ItemPredicate.Builder.item().of(item).build());
-    }
-
-    private static InventoryChangeTrigger.TriggerInstance unlockedByItemTag(TagKey<Item> tag) {
-        return inventoryTrigger(ItemPredicate.Builder.item().of(tag).build());
-    }
 
     public static void generateRecipes(RegistrateRecipeProvider recipeProvider) {
         ShapedRecipeBuilder
                 .shaped(RecipeCategory.TRANSPORTATION, AItems.AUTOMATED_DRIVER)
-                .unlockedBy("has_item", unlockedByItem(AllItems.PRECISION_MECHANISM))
+                .unlockedBy("has_item", RecipeProvider.has(AllItems.PRECISION_MECHANISM))
                 .define('A', AllItems.ANDESITE_ALLOY)
                 .define('G', AllItems.GOLDEN_SHEET)
                 .define('P', AllItems.PRECISION_MECHANISM)
@@ -42,7 +28,7 @@ public class GenCraftingRecipes {
 
         ShapedRecipeBuilder
                 .shaped(RecipeCategory.TRANSPORTATION, ABlocks.AUTOMATED_DRIVING_MODULE.asItem())
-                .unlockedBy("has_item", unlockedByItem(AItems.AUTOMATED_DRIVER))
+                .unlockedBy("has_item", RecipeProvider.has(AItems.AUTOMATED_DRIVER))
                 .define('C', AItems.CHARGED_COBALT_INGOT)
                 .define('D', AItems.AUTOMATED_DRIVER)
                 .define('S', AllItems.STURDY_SHEET)
