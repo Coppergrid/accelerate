aa05adc533e165174ca08a50f91866b68b082a0b
CircuitScoutDev <134339728+CircuitScoutDev@users.noreply.github.com>
2026-08-29 14:14:35 +0100

    Datagen most track blockstates for maglev and high speed

--- fabric/src/main/java/coppergrid/accelerate/fabric/datagen/CustomTrackBlockStateGenerator.java
+++ fabric/src/main/java/coppergrid/accelerate/fabric/datagen/CustomTrackBlockStateGenerator.java
@@ -1,5 +1,6 @@
 package coppergrid.accelerate.fabric.datagen;
 
+import com.simibubi.create.Create;
 import com.simibubi.create.content.trains.track.TrackBlock;
 import com.simibubi.create.content.trains.track.TrackShape;
 import com.simibubi.create.foundation.data.SpecialBlockStateGen;
@@ -10,6 +11,11 @@ import coppergrid.accelerate.Accelerate;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import io.github.fabricators_of_create.porting_lib.models.generators.ModelFile; // FABRIC ONLY
+import io.github.fabricators_of_create.porting_lib.models.generators.block.BlockModelBuilder;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CustomTrackBlockStateGenerator extends SpecialBlockStateGen {
     private final String path;
@@ -37,8 +43,56 @@ public class CustomTrackBlockStateGenerator extends SpecialBlockStateGen {
         if (value == TrackShape.NONE)
             return prov.models()
                     .getExistingFile(prov.mcLoc("block/air"));
-        return prov.models()
-                .getExistingFile(Accelerate.asResource("block/track/" + path + "/" + value.getModel()));
+        Map<String, String> textureMap = getTextureMap(value);
+
+        BlockModelBuilder builder = prov.models()
+                .withExistingParent(
+                        "block/track/" + path + "/" + value.getModel(),
+                        Create.asResource("block/track/" + value.getModel())
+                )
+                .texture(
+                        "particle",
+                        Accelerate.asResource("block/raw_cobalt_block")
+                );
+
+        for (Map.Entry<String, String> entry : textureMap.entrySet()) {
+            builder.texture(
+                    entry.getKey(),
+                    Accelerate.asResource("block/" + entry.getValue())
+            );
+        }
+
+        return builder;
+    }
+
+    private @NotNull Map<String, String> getTextureMap(TrackShape value) {
+        Map<String, String> textureMap = new HashMap<>();
+
+        switch (value) {
+            case TE, TN, TS, TW -> {
+                textureMap.put("1", path + "_track");
+                textureMap.put("2", path + "_track_mip");
+                textureMap.put("3", path + "_track");
+            }
+
+            case AE, AW, AN, AS -> {
+                textureMap.put("0", path + "_track");
+                textureMap.put("1", path + "_track_mip");
+            }
+
+            case CR_O, XO, ZO -> {
+                textureMap.put("1", path + "_track");
+                textureMap.put("2", path + "_track_mip");
+                textureMap.put("3", path + "_track_crossing");
+            }
+
+            default -> {
+                textureMap.put("0", path + "_track");
+                textureMap.put("1", path + "_track_mip");
+                textureMap.put("2", path + "_track_crossing");
+            }
+        }
+        return textureMap;
     }
 
 }
