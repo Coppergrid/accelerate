b789b957b746ce72570bdded57e9856875051897
CircuitScoutDev <134339728+CircuitScoutDev@users.noreply.github.com>
2026-08-31 12:27:12 +0100

    Add material override for maglev

--- common/src/main/java/coppergrid/accelerate/content/track/MaglevTrackBlock.java
+++ common/src/main/java/coppergrid/accelerate/content/track/MaglevTrackBlock.java
@@ -1,10 +1,16 @@
 package coppergrid.accelerate.content.track;
 
 import com.simibubi.create.content.trains.track.TrackBlock;
+import com.simibubi.create.content.trains.track.TrackMaterial;
 import coppergrid.accelerate.registry.ATrackMaterials;
 
 public class MaglevTrackBlock extends TrackBlock {
     public MaglevTrackBlock(Properties properties) {
         super(properties, ATrackMaterials.MAGLEV);
+    }
+
+    @Override
+    public TrackMaterial getMaterial() {
+        return ATrackMaterials.MAGLEV;
     }
 }
